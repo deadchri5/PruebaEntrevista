9279f4b8852baec2e8e2068d819816e2f1d1d9aa
deadchri5h <deadchri5h@gmail.com>
2021-09-30 18:20:09 -0500

    remove footer section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css'
 //Components
 import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
 import TaskInterface from './components/TaskInterface/TaskInterface';
 
 //Comentario desde rama dev
